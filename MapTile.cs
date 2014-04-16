d39fe5c48d35037f72c0d8135fd7977f5d52175b
Gunhee Lee <teuksu00@gmail.com>
2014-04-16 17:07:29 +0900

    Terrain XML
    Created blank template of terrain.xml and terrain.xsd. Further Terrain
    implementaion is coming soon.

---
+++ MapTile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTROPY
+{
+    // A hex-tile in GameBoard
+    class MapTile
+    {
+        Terrain terrain_type;
+    }
+
+    // A terrain has 5 layers: space, sky, surface, shallow, deep
+    class Terrain
+    {
+        enum Layers{Deep, Shallow, Surface, Sky, Space};
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ A Turn-Based Strategy game for web browsers.
 VS2013, C#.
 
 DesignDocument.txt - Most design specifics are in it.
+
+Simple coding convention
+  - ClassName
+  - functionName
+  - member_variable_name
+  - local_variable
